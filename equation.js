75f3c98e936749d32ee09df30ebd14cba4bab167
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-02-08 22:13:24 +0200

    created interface to read user input
    required 'readline' to read inputs from users

--- equation.js
+++ equation.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Write a number\n', function(num) {
+    console.dir(num)
+})
