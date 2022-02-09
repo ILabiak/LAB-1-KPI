4e31ed1b93e8e1b4f64789831f96d7eb60191d28
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-02-09 15:27:34 +0200

    made func to get quadric equation roots

--- equation.js
+++ equation.js
@@ -3,18 +3,18 @@
 const readline = require("readline");
 const fs = require("fs");
 
-const rl = readline.createInterface({
+/* const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-rl.question(
+ */
+/* rl.question(
   "Choose mode\n1) Interactive mode\n2) File mode\nAny other key - exit\n",
   function (mode) {
     if (mode === "1") interactiveFunc();
     if (mode === "2") console.log("mode 2");
   }
-);
+); */
 
 const interactiveFunc = async () => {
   console.log("Quadric equation instance: ax^2 + bx + c = 0");
@@ -34,7 +34,7 @@ const questionFunc = async (name) => {
           console.log(
             `Error. Expected a valid real number, got ${inp} instead. Please write number...\n`
           );
-          resolve (await questionFunc(name));
+          resolve(await questionFunc(name));
         } else {
           resolve(res);
         }
@@ -42,3 +42,15 @@ const questionFunc = async (name) => {
     );
   });
 };
+
+const solveQuadricEquation = (a, b, c) => {
+  const result = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  const result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  if (isNaN(result)) return "There are 0 roots";
+  if (result == result2) return `There is 1 root\nx1 = ${result}`;
+  return `There are 2 roots
+x1 = ${result}
+x2 = ${result2}`;
+};
+
+console.log(solveQuadricEquation(1, 4, 5));
