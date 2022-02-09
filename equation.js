7eb05a781bbeb64b71102cab94b431a1e0d879e2
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-02-09 15:30:55 +0200

    made func to get quadratic equation roots

--- equation.js
+++ equation.js
@@ -34,7 +34,7 @@ const questionFunc = async (name) => {
           console.log(
             `Error. Expected a valid real number, got ${inp} instead. Please write number...\n`
           );
-          resolve (await questionFunc(name));
+          resolve(await questionFunc(name));
         } else {
           resolve(res);
         }
@@ -42,3 +42,14 @@ const questionFunc = async (name) => {
     );
   });
 };
+
+const solveQuadraticEquation = (a, b, c) => {
+  const result = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  const result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  if (isNaN(result)) return "There are 0 roots";
+  if (result == result2) return `There is 1 root\nx1 = ${result}`;
+  return `There are 2 roots
+x1 = ${result}
+x2 = ${result2}`;
+};
+
